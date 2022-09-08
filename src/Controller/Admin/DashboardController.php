229c007ccb6080209a42c2bb3440aea2f7f0c5c3
NGUE EMMANUEL <ngueemmanuel.prof@gmail.com>
2022-09-08 12:36:01 +0100

    easyadmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        //return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        return $this->render('admin/index.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('K-PITAL SOLUTIONS');
+    }
+
+    // public function configureMenuItems(): iterable
+    // {
+
+    //     return [
+    //         MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+    //         MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class),
+    //         MenuItem::linkToCrud ('Projets', 'fa fa-tarp',Projet::class),
+    //         MenuItem::section  ('',''),
+
+    //         MenuItem::subMenu('Blog', 'fas fa-list')->setSubItems([
+    //             MenuItem::linkToCrud('Thématiques blog', 'fa fa-tags', CategoryPost::class),
+    //             MenuItem::linkToCrud('Posts', 'fa fa-file-text', Post::class),
+    //         ]),
+    //         MenuItem::subMenu('Services', 'fas fa-list')->setSubItems([
+    //              MenuItem::linkToCrud('Categorie', 'fa fa-tags', ServiceCategory::class),
+    //              MenuItem::linkToCrud('Les Services', 'fas fa-newspaper', Service::class),
+    //             MenuItem::linkToCrud ('Clients','fas fa-user', Client::class)
+    //         ]),
+    //         MenuItem::section  ('',''),
+    //         MenuItem::linkToCrud('Courriel', 'fas fa-envelope', Contact::class)
+    //         // ...
+    //     ];
+    // }
+}
