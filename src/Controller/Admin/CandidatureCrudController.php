6910e1cf6f7326c811023c938ede3043b72e47f3
mintoua <toupkandi.mintoua@esprit.tn>
2022-09-19 01:38:40 +0100

    accept and decline application from admin

--- src/Controller/Admin/CandidatureCrudController.php
+++ src/Controller/Admin/CandidatureCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Admin\Field\PdfField;
+use App\Entity\CandidateResume;
 use App\Entity\Candidature;
+use App\Services\MailSender;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -17,25 +19,110 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class CandidatureCrudController extends AbstractCrudController
 {
 
+    public function __construct (
+        private EntityManagerInterface $entityManager,
+        private MailSender $sender,
+        private AdminUrlGenerator $adminUrlGenerator
+    )
+    {
+    }
+
     public function configureActions ( Actions $actions ) : Actions
     {
+        $hireCandidate = Action::new ('hireCandidate','Accepté la candidature')
+            ->linkToCrudAction ('hireCandidate');
+        $hireNotCandidate = Action::new ('hireNotCandidate','Réfusé la candidature')
+            ->linkToCrudAction ('hireNotCandidate');
 
         return $actions
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
             ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
             ->add(Crud::PAGE_EDIT, Action::INDEX)
             ->add(Crud::PAGE_NEW, Action::INDEX)
+            ->add ('detail',$hireCandidate)
+            ->add ('detail',$hireNotCandidate)
             ->disable(Action::NEW);
     }
 
+    public function urlDispatcher(String $action){
+        $url = $this->adminUrlGenerator
+            ->setDashboard(DashboardController::class)
+            ->setController(OrderCrudController::class)
+            ->setAction($action)
+            ->generateUrl();
+
+        return $url;
+    }
 
     public static function getEntityFqcn(): string
     {
         return Candidature::class;
+    }
+
+    public function hireCandidate(AdminContext $context){
+
+        $candidature = $context->getEntity ()->getInstance ();
+        $candidateResume = $this->entityManager->getRepository (CandidateResume::class)->find (
+            $candidature->getCandidateResume()->getId()
+        );
+
+        if (!$candidateResume){
+            throw $this->createNotFoundException ("Erreur! Ce profil n'exite pas!");
+        }
+
+        $candidature->setIsHired(true);
+
+        $content = "Bonjour ".$candidateResume->getNomcomplet()."<br> Nous avons bien étudié votre candidature pour un poste de "
+            .$candidature->getJob()->getTitle().' '."et nous vous en remercions.<br><br>";
+        $content .="Nous avons donc le plaisir de vous informer que votre candidature a été retenue.";
+
+        $this->sender->send(
+            $candidateResume->getEmail(),
+            $candidateResume->getNomcomplet(),
+            $content,
+            "Réponse à votre candidature chez CAPITAL RH"
+        );
+
+        $this->entityManager->flush ();
+
+        $this->addFlash('notice',"Candidature mise à jour");
+        return $this->redirect ($this->urlDispatcher (Action::INDEX));
+    }
+
+    public function hireNotCandidate(AdminContext $context){
+
+        $candidature = $context->getEntity ()->getInstance ();
+        $candidateResume = $this->entityManager->getRepository (CandidateResume::class)->find (
+            $candidature->getCandidateResume()->getId()
+        );
+
+        if (!$candidateResume){
+            throw $this->createNotFoundException ("Erreur! Ce profil n'exite pas!");
+        }
+
+        $candidature->setIsHired(false);
+
+        $content = "Bonjour ".$candidateResume->getNomcomplet()."<br> Nous avons bien étudié votre candidature pour un poste de "
+            .$candidature->getJob()->getTitle().' '."et nous vous en remercions.<br><br>";
+        $content .="Malheureusement, nous ne pouvons donner suite à votre candidature.
+                    Nous vous remercions cependant du temps que vous nous avez consacré et de l’intérêt que vous portez à notre entreprise";
+
+        $this->sender->send(
+            $candidateResume->getEmail(),
+            $candidateResume->getNomcomplet(),
+            $content,
+            "Réponse à votre candidature chez CAPITAL RH"
+        );
+
+        $this->entityManager->flush ();
+
+        $this->addFlash('notice',"Candidature mise à jour");
+        return $this->redirect ($this->urlDispatcher (Action::INDEX));
     }
 
     public function configureCrud(Crud $crud): Crud
